07ee07029c8815840947ffdecb0b0e0150214f44
Ian Roberts <136416836+IanRoberts092@users.noreply.github.com>
2023-11-09 12:32:14 -0600

    Lab Mostly Finished Update
    The long msg decryption is like 90% accurate, but some letters are off for whatever reason. Everything else is working as intended.

--- src/Encrypter.java
+++ src/Encrypter.java
@@ -2,12 +2,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
-
 public class Encrypter {
-
     private int shift;
     private String encrypted;
-
     /**
      * Default Constructor
      */
@@ -15,7 +12,6 @@ public class Encrypter {
         this.shift = 1;
         this.encrypted = "";
     }
-
     /**
      * Non-default Constructor
      * @param s - custom shift amount
@@ -24,7 +20,6 @@ public class Encrypter {
         this.shift = s;
         this.encrypted = "";
     }
-
     /**
      * Encrypts the content of a file and writes the result to another file.
      *
@@ -33,9 +28,14 @@ public class Encrypter {
      * @throws Exception if an error occurs while reading or writing the files
      */
     public void encrypt(String inputFilePath, String encryptedFilePath) throws Exception {
-        //TODO: Call the read method, encrypt the file contents, and then write to new file
+        String content = readFile(inputFilePath);
+        encrypted = "";
+        for (char ch : content.toCharArray()){
+            encrypted += encryptCharacter(ch, shift);
+        }
+        writeFile(encrypted, encryptedFilePath);
+        //Call the read method, encrypt the file contents, and then write to new file
     }
-
     /**
      * Decrypts the content of an encrypted file and writes the result to another file.
      *
@@ -44,9 +44,14 @@ public class Encrypter {
      * @throws Exception if an error occurs while reading or writing the files
      */
     public void decrypt(String messageFilePath, String decryptedFilePath) throws Exception {
-        //TODO: Call the read method, decrypt the file contents, and then write to new file
+        String encryptedContent = readFile(messageFilePath);
+        String decrypted = "";
+        for (char ch : encryptedContent.toCharArray()){
+            decrypted += encryptCharacter(ch, -shift);
+        }
+        writeFile(decrypted, decryptedFilePath);
+        //Call the read method, decrypt the file contents, and then write to new file
     }
-
     /**
      * Reads the content of a file and returns it as a string.
      *
@@ -55,27 +60,45 @@ public class Encrypter {
      * @throws Exception if an error occurs while reading the file
      */
     private static String readFile(String filePath) throws Exception {
-        String message = "";
-        //TODO: Read file from filePath
-        return message;
+        StringBuilder message = new StringBuilder();
+        File file  = new File(filePath);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()){
+            message.append(scanner.nextLine());
+            if (scanner.hasNextLine()){
+                message.append("\n");
+            }
+        }
+        scanner.close();
+        return message.toString();
     }
-
     /**
      * Writes data to a file.
      *
      * @param data     the data to be written to the file
      * @param filePath the path to the file where the data will be written
      */
-    private static void writeFile(String data, String filePath) {
-        //TODO: Write to filePath
+    private static void writeFile(String data, String filePath) throws IOException {
+        FileWriter writer = new FileWriter(filePath);
+        writer.write(data);
+        writer.close();
     }
-
     /**
      * Returns a string representation of the encrypted text.
      *
      * @return the encrypted text
      */
-    @Override
+    private char encryptCharacter(char character, int shift){
+        if (Character.isLetter(character)){
+            char base = Character.isLowerCase(character) ? 'a' : 'A';
+            int offset = Character.toLowerCase(character) - base + shift;
+            int modOffset = Math.floorMod(offset, 26); // returns a not negative//the lowest modulus of the offset and alphabet
+            return (char) (base + modOffset);
+        }
+        else {
+            return character;
+        }
+    }
     public String toString() {
         return encrypted;
     }
--- src/EncryptionTester.java
+++ src/EncryptionTester.java
@@ -2,12 +2,16 @@
 public class EncryptionTester {
 	
 	public static void main(String[] args) throws Exception {
-        String encryptedFilePath = "src/encrypted.txt";
-        String decryptedFilePath = "src/decrypted.txt";
-        String inputFilePath = "src/encryptMe.txt";
-        String outputFilePath = "src/newEncrypted.txt";
+
+        System.out.println("Working Directory = " + System.getProperty("user.dir")); // Your directory to save you time! :)
+
+
+        String encryptedFilePath = "C:\\Users\\ianth\\Desktop\\JavaHW\\FA23-CS203-Lab11-main\\FA23-CS203-Lab11-main\\src\\encrypted.txt";
+        String decryptedFilePath = "C:\\Users\\ianth\\Desktop\\JavaHW\\FA23-CS203-Lab11-main\\FA23-CS203-Lab11-main\\src\\decrypted.txt";
+        String inputFilePath = "C:\\Users\\ianth\\Desktop\\JavaHW\\FA23-CS203-Lab11-main\\FA23-CS203-Lab11-main\\src\\encryptMe.txt";
+        String outputFilePath = "C:\\Users\\ianth\\Desktop\\JavaHW\\FA23-CS203-Lab11-main\\FA23-CS203-Lab11-main\\src\\newEncrypted.txt";
 		
-        int shift = 1; // Says how much you would like to shift
+        int shift = 4; // Says how much you would like to shift
 
         Encrypter enc = new Encrypter(shift);
 
